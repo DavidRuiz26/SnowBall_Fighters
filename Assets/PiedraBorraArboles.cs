c9801aa0e2eed81b58854126c67fe617ccbfbfbd
LUIS PAÑOS CUENCA <Luis.Panos@alu.uclm.es>
2023-05-30 19:08:40 +0200

    Introducidos los pinwinos y puestos los power up

--- Assets/PiedraBorraArboles.cs
+++ Assets/PiedraBorraArboles.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PiedraBorraArboles : MonoBehaviour
 {
+    private int rompe= 0; 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Obstaculo"))
         {
             Destroy(collision.gameObject);
+            rompe++; 
         }
+        if (rompe == 3)
+        {
+            Destroy(gameObject); 
+        }
+
     }
 }
